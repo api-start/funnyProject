571c93f54e3e132ace3dee3a4ad0191c0f5213ba
EloyTejero <tejero.eloyet36@gmail.com>
2025-07-21 20:28:52 -0300

    fix: temporary test eliminated and added tags to Apiimplementation repository

--- src/main/java/com/project/api/repository/ApiDataAccessService.java
+++ src/main/java/com/project/api/repository/ApiDataAccessService.java
@@ -2,7 +2,9 @@ package com.project.api.repository;
 
 import com.project.api.model.Api;
 import com.project.api.model.Category;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
 import java.sql.Date;
 import java.sql.PreparedStatement;
@@ -11,10 +13,12 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository("postgres")
 public class ApiDataAccessService implements ApiRepository{
 
     private final JdbcTemplate jdbcTemplate;
 
+    @Autowired
     public ApiDataAccessService(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
