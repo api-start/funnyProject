4a1a1f4827d85f62df48235d82de554a01d47da7
santinozanone <santizanone@gmail.com>
2025-07-22 19:02:31 -0300

    feat: added exception handling for ApiAlreadyCreatedException in globalExceptionHandler

--- src/main/java/com/project/api/configuration/GlobalExceptionHandler.java
+++ src/main/java/com/project/api/configuration/GlobalExceptionHandler.java
@@ -1,11 +1,23 @@
 package com.project.api.configuration;
 
 
+import com.project.api.service.exception.ApiAlreadyCreatedException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 
 @ControllerAdvice
 public class GlobalExceptionHandler  {
+
+    @ExceptionHandler(ApiAlreadyCreatedException.class)
+    public ProblemDetail handleApiAlreadyCreatedException(ApiAlreadyCreatedException e){
+        ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
+        problemDetail.setTitle("Bad request");
+        problemDetail.setDetail("The api already exists");
+        return problemDetail;
+    }
 
 
 }
